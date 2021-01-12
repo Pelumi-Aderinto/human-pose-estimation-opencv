cd9f82800de37602e4cbc0a09ac2e53d439a7ba8
Pelumi Aderinto <64173848+Pelumi-Aderinto@users.noreply.github.com>
2021-01-12 07:51:45 +0100

    Update openpose.py

--- openpose.py
+++ openpose.py
@@ -76,6 +76,6 @@ while cv.waitKey(1) < 0:
 
     t, _ = net.getPerfProfile()
     freq = cv.getTickFrequency() / 1000
-    cv.putText(frame, '%.2fms' % (t / freq), (10, 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+   # cv.putText(frame, '%.2fms' % (t / freq), (10, 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
 
     plt.imshow(frame)
