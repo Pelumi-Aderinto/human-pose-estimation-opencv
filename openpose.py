0c9ee8570211d1cad219109a1b6a2b398ca778f6
Pelumi Aderinto <64173848+Pelumi-Aderinto@users.noreply.github.com>
2021-01-12 08:07:53 +0100

    Update openpose.py

--- openpose.py
+++ openpose.py
@@ -80,4 +80,4 @@ def PoseDetector(frame):
    # freq = cv.getTickFrequency() / 1000
    # cv.putText(frame, '%.2fms' % (t / freq), (10, 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))    
     return frame  
-plt.imshow(frame)
+
